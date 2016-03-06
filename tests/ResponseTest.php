3fd42c6c7943874dd37b6777be077b28dff4da39
John Kirk <jkirk@ginkgostreet.com>
2016-03-06 16:05:18 +0000

    Added tests. These were made during the CiviSprint but not committed for some reason.

---
+++ tests/ResponseTest.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file   Tests the response (Processor) part of the SMS survey system
+ * @author David Knoll <david@futurefirst.org.uk>
+ */
+
+require_once 'TestUtils.php';
+
+class ResponseTest extends PHPUnit_Framework_TestCase {
+  
+}
